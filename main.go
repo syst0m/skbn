3809d02b9b026cdbde5796cf8eef3b3fb97c8bd7
Tomislav Tomasic <tomislav.tomasic@underwriteme.co.uk>
2022-06-29 16:34:08 +0100

    master: Fix cmd package reference

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	cmd := NewRootCmd(os.Args[1:])
+	cmd := cmd.NewRootCmd(os.Args[1:])
 	if err := cmd.Execute(); err != nil {
 		log.Fatal("Failed to execute command")
 	}
